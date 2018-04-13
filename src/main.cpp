9c910f60ff8d525eac26434386d55556c3511b9b
Kevin Tung <kevin.tung@mavs.uta.edu>
2018-04-13 15:05:36 -0500

    changed gui_window to main_window

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,13 @@
 #include "classes/container/container.h"
 #include "classes/controller/controller.h"
 #include "classes/emporium/emporium.h"
-#include "classes/gui_window/gui_window.h"
+#include "classes/main_window/main_window.h"
 
 int main(int argc, char* argv[]) {
     auto app = Gtk::Application::create(argc, argv, "com.kevinxtung.mice");
     Emporium emporium;
     Controller controller(emporium);
-    GUI_Window window(&controller);
+    Main_Window window(&controller);
     window.set_title("Test GUI");
 
     return app->run(window);
