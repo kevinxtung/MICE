e09973f9c016d0789692c1c0c5fac7ad09050916
Kevin Tung <kevin.tung@mavs.uta.edu>
2018-04-15 14:21:51 -0500

    Working screen swapping!

---
+++ src/classes/main_window/main_window.cpp
@@ -0,0 +1,89 @@
+#include "main_window.h"
+
+//****************//
+//*GUI MANAGEMENT*//
+//****************//
+
+// Global GUI Elements
+Gtk::Box* vbox = NULL;
+Gtk::Toolbar* toolbar = NULL;
+
+Main_Window::Main_Window(Controller* controller) : m_controller(controller) {
+
+    // GUI SETUP
+    set_default_size(1366, 768);
+    fullscreen();
+
+    vbox = Gtk::manage(new Gtk::Box(Gtk::ORIENTATION_VERTICAL, 0));
+    add(*vbox);
+    toolbar = Gtk::manage(new Gtk::Toolbar);
+    defaultScreen();
+}
+
+void Main_Window::clean() {
+    vbox->remove(*toolbar);
+    toolbar = Gtk::manage(new Gtk::Toolbar);
+}
+
+Main_Window::~Main_Window() { }
+
+//*********//
+//*SCREENS*//
+//*********//
+
+void Main_Window::defaultScreen() {
+    clean();
+    vbox->add(*toolbar);
+
+    i_order = Gtk::manage(new Gtk::Image{"randomicon.png"});
+    b_order = Gtk::manage(new Gtk::ToolButton(*i_order));
+    b_order->signal_clicked().connect(sigc::mem_fun(*this, &Main_Window::onOrderClick));
+    toolbar->append(*b_order);
+
+    Gtk::SeparatorToolItem *sep = Gtk::manage(new Gtk::SeparatorToolItem());
+    sep->set_expand(true);
+    toolbar->append(*sep);
+
+    i_employee = Gtk::manage(new Gtk::Image{"employeescreenbutton.png"});
+    b_employee = Gtk::manage(new Gtk::ToolButton(*i_employee));
+    b_employee->signal_clicked().connect(sigc::mem_fun(*this, &Main_Window::onEmployeeClick));
+    toolbar->append(*b_employee);
+
+    vbox->show_all();
+}
+
+void Main_Window::employeeScreen() {
+    clean();
+    vbox->add(*toolbar);
+    
+    i_back = Gtk::manage(new Gtk::Image{"backbutton.png"});
+    b_back = Gtk::manage(new Gtk::ToolButton(*i_back));
+    b_back->signal_clicked().connect(sigc::mem_fun(*this, &Main_Window::defaultScreen));
+    toolbar->append(*b_back);
+
+    //Gtk::Toolbar *toolbar2 = Gtk::manage(new Gtk::Toolbar);
+
+    i_createItem = Gtk::manage(new Gtk::Image{"createitem.png"});
+    b_createItem = Gtk::manage(new Gtk::ToolButton(*i_createItem));
+    b_createItem->signal_clicked().connect(sigc::mem_fun(*this, &Main_Window::onCreateItemClick));
+    toolbar->append(*b_createItem);
+
+    vbox->show_all();
+}
+
+//***********//
+//*CALLBACKS*//
+//***********//
+
+
+void Main_Window::onOrderClick() {
+    m_controller->execute(1);
+}
+
+void Main_Window::onCreateItemClick() {
+    m_controller->execute(1);
+}
+
+void Main_Window::onEmployeeClick() {
+    employeeScreen();
+}
---
+++ src/classes/main_window/main_window.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <gtkmm.h>
+#include "classes/emporium/emporium.h"
+#include "classes/controller/controller.h"
+
+class Main_Window : public Gtk::Window {
+    public:
+	Main_Window(Controller* controller);
+	void clean();
+	virtual ~Main_Window();
+
+    protected:
+	// Screens
+	void defaultScreen();
+	void employeeScreen();
+
+	// Callbacks
+	void onOrderClick();
+	void onEmployeeClick();
+	void onCreateItemClick();
+
+    private:
+	Controller* m_controller;
+	
+	//************************//
+	//*Customer Functionality*//
+	//************************//
+
+	// Create/Start Order
+	Gtk::ToolButton *b_order;
+	Gtk::Image *i_order;
+
+	//************************//
+	//*Employee Functionality*//
+	//************************//
+
+	// Switch to Employee Mode
+	Gtk::ToolButton *b_employee;
+	Gtk::Image *i_employee;
+
+	// Back to Customer Mode
+	Gtk::ToolButton *b_back;
+	Gtk::Image *i_back;
+
+	// Create Item
+	Gtk::ToolButton *b_createItem;
+	Gtk::Image *i_createItem;
+
+};
