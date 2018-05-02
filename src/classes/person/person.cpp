b81b76b193409c61e7c3040f50cd4317f1847fe2
Kevin Tung <kevin.tung@mavs.uta.edu>
2018-05-01 22:00:14 -0500

    working but mem crash

--- src/classes/person/person.cpp
+++ src/classes/person/person.cpp
@@ -1,15 +1,15 @@
 #include "person.h"
 #include <iostream>
-#include <cmath>
 
 Person::Person(std::string name, std::string phone) : m_name{name}, m_phone{phone}, m_active{true} {
-    std::string filteredNumber = "";
+    std::cout << "HERE" << std::endl;
     for (int x = 0; x < phone.size(); x++) {
         if (phone[x] != '-') {
-            filteredNumber += phone[x];
+            m_ID += phone[x];
+            std::cout << "CAT A " << phone[x] << std::endl;
         }
     }
-    m_ID = filteredNumber;
+    std::cout << "DONE WITH " << m_ID << std::endl;
 }
 
 std::string Person::getName() {return m_name;}
